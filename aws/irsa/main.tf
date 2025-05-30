387130855e880a7ad5b0476042c97d026834c2ff
tae2089 <tae2089002@gmail.com>
2025-05-30 19:37:29 +0900

    update irsa module

--- aws/irsa/main.tf
+++ aws/irsa/main.tf
@@ -1,25 +1,47 @@
 data "aws_caller_identity" "current" {}
 
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect = "Allow"
+    principals {
+      type = "Federated"
+      identifiers = [var.config.eks_oidc_arn]
+    }
+    condition {
+      test = "StringEquals"
+      variable = "${var.config.eks_oidc_issuer}:sub"
+      values = ["system:serviceaccount:${var.config.service_account.namespace}:${var.config.service_account.name}"]
+    }
+    condition {
+      test = "StringEquals"
+      variable = "${var.config.eks_oidc_issuer}:aud"
+      values = ["sts.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "this" {
   name                = var.config.name
-  managed_policy_arns = var.config.policy_arns
-
-  assume_role_policy = jsonencode({
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${var.config.eks_issuer_url}"
-        }
-        Condition = {
-          StringEquals = {
-            "${var.config.eks_issuer_url}:aud" = "sts.amazonaws.com",
-            "${var.config.eks_issuer_url}:sub" = "system:serviceaccount:${var.config.service_account.namespace}:${var.config.service_account.name}"
-          }
-        }
-      },
-    ]
-  })
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
   tags = var.config.tags
+}
+
+resource "aws_iam_role_policy_attachment" "managed_policy" {
+  for_each = { for policy in var.config.policy_arns : policy => policy }
+  role = aws_iam_role.this.name
+  policy_arn = each.value
+}
+
+resource "aws_iam_policy" "custom_policy" {
+    for_each = {for policy in var.config.inline_policies : policy.name => policy}
+    name = each.value.name
+    policy = file("${each.value.policy_file}")
+}
+
+resource "aws_iam_role_policy_attachment" "custom_policy" {
+    for_each = {for policy in var.config.inline_policies : policy.name => policy}
+    role = aws_iam_role.this.name
+    policy_arn = aws_iam_policy.custom_policy[each.value.name].arn
 }
--- aws/irsa/variable.tf
+++ aws/irsa/variable.tf
@@ -1,13 +1,17 @@
 variable "config" {
   type = object({
     name           = string
-    policy_arns    = optional(list(string))
-    cluster_name   = string
-    eks_issuer_url = string
+    eks_oidc_arn   = string
+    eks_oidc_issuer = string
+    policy_arns    = optional(list(string),[])
     service_account = object({
       name      = string
       namespace = string
     })
+    inline_policies = optional(list(object({
+      name = string
+      policy_file = string
+    })), [])
     tags = optional(map(string))
   })
 }
